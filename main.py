47dc4e17cfc57a63a6dd0cfb7100816c9f3ec7bc
ilayda21 <cavusogluilayda@gmail.com>
2021-01-05 20:42:49 +0300

    Basic game structure

--- main.py
+++ main.py
@@ -93,54 +93,6 @@ class BoxesGame:
     def mainloop(self):
         self.window.mainloop()
 
-    # ------------------------------------------------------------------
-    # Logical Functions:
-    # The modules required to carry out game logic
-    # ------------------------------------------------------------------
-
-    def convert_grid_to_logical_position(self, grid_position):
-        grid_position = np.array(grid_position)
-        position = (grid_position - distance_between_dots / 4) // (distance_between_dots / 2)
-
-        type = False
-        logical_position = []
-        if position[1] % 2 == 0 and (position[0] - 1) % 2 == 0:
-            r = int((position[0] - 1) // 2)
-            c = int(position[1] // 2)
-            logical_position = [r, c]
-            type = 'row'
-            # self.row_status[c][r]=1
-        elif position[0] % 2 == 0 and (position[1] - 1) % 2 == 0:
-            c = int((position[1] - 1) // 2)
-            r = int(position[0] // 2)
-            logical_position = [r, c]
-            type = 'col'
-
-        return logical_position, type
-
-    # ------------------------------------------------------------------
-    # Drawing Functions:
-    # The modules required to draw required game based object on canvas
-    # ------------------------------------------------------------------
-    #
-    # def make_edge(self, type, logical_position):
-    #     if type == 'row':
-    #         start_x = distance_between_dots / 2 + logical_position[0] * distance_between_dots
-    #         end_x = start_x + distance_between_dots
-    #         start_y = distance_between_dots / 2 + logical_position[1] * distance_between_dots
-    #         end_y = start_y
-    #     elif type == 'col':
-    #         start_y = distance_between_dots / 2 + logical_position[1] * distance_between_dots
-    #         end_y = start_y + distance_between_dots
-    #         start_x = distance_between_dots / 2 + logical_position[0] * distance_between_dots
-    #         end_x = start_x
-    #
-    #     if self.player1_turn:
-    #         color = player1_color
-    #     else:
-    #         color = player2_color
-    #     self.canvas.create_line(start_x, start_y, end_x, end_y, fill=color, width=edge_width)
-
     def refresh_board(self):
         for i in range(number_of_dots):
             x = i * distance_between_dots + distance_between_dots / 2
@@ -165,14 +117,6 @@ class BoxesGame:
                 self.canvas.create_oval(start_x - dot_width / 2, end_x - dot_width / 2, start_x + dot_width / 2,
                                         end_x + dot_width / 2, fill=color,
                                         outline=color)
-
-    def shade_box(self, box, color):
-        start_x = distance_between_dots / 2 + box[1] * distance_between_dots + edge_width / 2
-        start_y = distance_between_dots / 2 + box[0] * distance_between_dots + edge_width / 2
-        end_x = start_x + distance_between_dots - edge_width
-        end_y = start_y + distance_between_dots - edge_width
-        self.canvas.create_rectangle(start_x, start_y, end_x, end_y, fill=color, outline='')
-
 
 game_instance = BoxesGame()
 game_instance.mainloop()
